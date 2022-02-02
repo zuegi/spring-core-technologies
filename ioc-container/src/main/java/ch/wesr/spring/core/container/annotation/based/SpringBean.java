f56430b9355041fb7993ba45a7fd8aa506f5c741
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2022-02-02 14:34:23 +0100

    Spring IoC * include java code SpringBean

---
+++ ioc-container/src/main/java/ch/wesr/spring/core/container/annotation/based/SpringBean.java
@@ -0,0 +1,23 @@
+package ch.wesr.spring.core.container.annotation.based;
+
+public class SpringBean {
+
+    private String objectName;
+    private String klasse;
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public String getKlasse() {
+        return klasse;
+    }
+
+    public void setKlasse(String klasse) {
+        this.klasse = klasse;
+    }
+}
