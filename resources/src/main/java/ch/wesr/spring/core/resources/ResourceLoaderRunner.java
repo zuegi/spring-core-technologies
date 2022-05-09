d89c44afc186bc021011cd47964577707868fbdf
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2022-05-09 07:56:29 +0200

    2 Resources * kommentar ergänzt

--- resources/src/main/java/ch/wesr/spring/core/resources/ResourceLoaderRunner.java
+++ resources/src/main/java/ch/wesr/spring/core/resources/ResourceLoaderRunner.java
@@ -10,19 +10,19 @@ public class ResourceLoaderRunner {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         ResourceLoaderService loaderService = context.getBean(ResourceLoaderService.class);
-
+        // lies aus einer FilePath Resource
         try {
             Assertions.assertThat(loaderService.zeigeResourceDataMitFilePath()).contains("= 2 Resources");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+//        lies aus einer classpath resource
         try {
             Assertions.assertThat(loaderService.zeigeResourceAusDemClasspath()).contains("hallo ich bin der Inhalt eines hallo.txt files aus dem classpath");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        // lies aus einer http resource
         try {
             Assertions.assertThat(loaderService.zeigeResourcesAusWebSeite()).contains("Climate STABIO");
         } catch (IOException e) {
